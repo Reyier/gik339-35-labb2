89a7e8fb675166bb47def4ddc340769aef96f1a2
Reyier <adamreyier@gmail.com>
2023-12-13 11:55:39 +0100

    Update script.js

--- gik339-ht23-Labb2/client/script.js
+++ gik339-ht23-Labb2/client/script.js
@@ -1,17 +1,6 @@
 async function fetchDataFromServer() {
     try {
-      // 6,1 och 6,2: Använd fetch() till servern
-      const response = await fetch('http://localhost:3000/users');
-  
-      // Kontrollera om förfrågan lyckades eller inte
-      if (!response.ok) {
-        throw new Error('Något gick fel vid hämtning av data');
-      }
-  
-      // 6,4 och 6,5: svar till JSON-format
-      const data = await response.json();
-  
-      // 6,6: Skapa en array 
+      // Din kod för att hämta data från servern...
       const usersArray = data.map(user => ({
         id: user.id,
         firstName: user.firstName,
@@ -20,14 +9,15 @@ async function fetchDataFromServer() {
         color: user.color
       }));
   
-      // 6,7: Logga användarobjekten till konsolen för verifiering
       console.log('Användare som JavaScript-objekt:', usersArray);
-  
+      return usersArray;
     } catch (error) {
-      // Vid fel, logga felmeddelandet till konsolen
       console.error('Fetch error:', error.message);
+      return [];
     }
   }
   
   // Anropa funktionen för att hämta och behandla data från servern
-  fetchDataFromServer();
+  const retrievedUsers = await fetchDataFromServer();
+  console.log('Användare utanför fetchDataFromServer:', retrievedUsers);
+  
