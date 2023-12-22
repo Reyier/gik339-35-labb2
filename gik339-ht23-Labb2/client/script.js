f51abfa20b7c2963354f36d5e44d1996853929a1
Reyier <adamreyier@gmail.com>
2023-12-22 13:21:33 +0100

    update

--- gik339-ht23-Labb2/client/script.js
+++ gik339-ht23-Labb2/client/script.js
@@ -1,27 +1,49 @@
-// Platsen där du hämtar användardata och skapar listan
-// Tidigare kod för att hämta och bearbeta användarlistan från databasen
+
+
+
+// Uppgift 6 punkt 1 Gör en asynkron  GET-förfrågan till servern för att hämta användarinformation. 
+//-""- punkt2. För att använda detta objekt, använder du .then() för att säga "när förfrågan är klar. Vi valde det för vi sett det på lektionen. 
 fetch('http://localhost:3000/users')
+  // Konverterar svaret till JSON-format
   .then((response) => response.json())
+  // Hanterar den returnerade användarlistan
   .then((users) => {
-    const ul = document.createElement("ul");
-    ul.classList.add("userList");
+    // Hämtar referensen till HTML-elementet med klassen "container"
     const div = document.querySelector(".container");
-    div.appendChild(ul);
 
-  
+
+
+    // Skapar en ul-lista med Bootstrap
+    const userList = document.createElement("ul");
+    userList.classList.add("list-group");
+    div.appendChild(userList);
+
+    //createELement som skapar ny html kod som ligger i den andra .then functionen
+
+
+    
+    // Loopar igenom varje användarobjekt i den hämtade listan (users)
     users.forEach(user => {
-      const li = document.createElement("li");
-      li.style.backgroundColor = user.color;
-      
-      const firstName = user.firstName;
-      const lastName = user.lastName;
-      const userName = user.username;
+      // Skapar ett listelement (li) för varje användare
+      const listItem = document.createElement("li");
+      listItem.classList.add("list-group-item");
+      listItem.style.backgroundColor = user.color; // Anpassa bakgrundsfärgen baserat på användarens färg
 
-      const html = `<span>Name: ${firstName} ${lastName}</span><br>Username: ${userName}`;
-      
-      li.insertAdjacentHTML('beforeend', html);
-      ul.appendChild(li);
+      // Skapar en rubrik för användarens namn (h5-element)
+      const name = document.createElement("h5");
+      name.textContent = `${user.firstName} ${user.lastName}`;
+      name.classList.add("mb-1"); // Lägg till en Bootstrap-marginalklass
+
+      // Skapar en paragraph för användarnamn (p-element)
+      const username = document.createElement("p");
+      username.textContent = `Username: ${user.username}`;
+      username.classList.add("mb-1"); // Lägg till en Bootstrap-marginalklass
+
+      // Lägger till användarnamn och användarinformation i listelementet (li)
+      listItem.appendChild(name);
+      listItem.appendChild(username);
+
+      // Lägger till listelementet (li) i listan (ul)
+      userList.appendChild(listItem);
     });
   });
-
-  
--- gik339-ht23-Labb2/server/server.js
+++ gik339-ht23-Labb2/server/server.js
@@ -1,40 +1,57 @@
+// uppgift 2 punkt 1. Importerar Express och SQLite-moduler
 const express = require("express");
+
+
+
+//npm install sqlite3 för installering i terminal
+//uppgift 3 Importerar SQLite3 
 const sqlite3 = require("sqlite3").verbose();
 
+
+
+// Uppgift 2.  Skapar en Express-serverinstans
 const server = express();
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "*");
+
+server.use(express.json()); // Hantera JSON-data
+server.use(express.urlencoded({ extended: false })); // Hantera URL-kodad data
+server.use((req, res, next) => { // Hantera CORS-headers
+  res.header("Access-Control-Allow-Origin", "*"); // Tillåt alla domäner
+  res.header("Access-Control-Allow-Headers", "*"); // Tillåt alla headers
+  res.header("Access-Control-Allow-Methods", "*"); // Tillåt alla metoder
   next();
 });
 
+
+
+
+
+
+
+// Uppgift 3: Skapa en databaskoppling
 const db = new sqlite3.Database("./gik339-labb2.db");
 
+// uppgift 2 punkt 3 och 4 : Definierar en GET-endpoint för "/users"
 server.get("/users", (req, res) => {
-  const query = "SELECT * FROM users";
+  const query = "SELECT * FROM users"; // SQL-fråga för att hämta alla användare
 
+  // Kör SQL-frågan på databasen
   db.all(query, (err, rows) => {
     if (err) {
+      // vid fel skickas en felstatus och ett meddelande
       res.status(500).send(err.message);
     } else {
-      res.json(rows); 
+      //  Rad som skickar response till klienten ssom gjorde förfrågan
+      // Objeketet res.
+      res.json(rows);
     }
   });
 });
 
+//uppgift 2 punkt 2: Ange vilken port servern ska lyssna på och starta servern
 const port = 3000;
 server.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
 
 
-// Uppgift 4 Testa Backend 
-// Uppgift 5 
-
-
-
-// test för Content security policy
